52ec80770e83c32e2322c1286bc27b1ceee929fd
Arbusta <cesar.sanchez@arbusta.net>
2022-07-22 20:53:19 -0300

    Ejercicios Primera parte

---
+++ CesarSanchez/Condicionales/ejercicios.py
@@ -0,0 +1,10 @@
+# 1.- Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int (input("Escriba su edad: "))
+mayorEdad = 18
+
+if edad >= mayorEdad:
+    print ("Es mayor de edad")
+
+else: 
+    print ("No es mayor de edad")
